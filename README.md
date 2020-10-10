1f7a91c0d84fb63856a456d7761e22d3ca1056be
dreadsystems <dread.systems@gmail.com>
2020-10-10 10:10:27 -0400

    Add project files

---
+++ README.md
@@ -0,0 +1,4 @@
+# ProcessHollowing
+A simple implementation of process hollowing to go along with a blog post.
+
+Will paste link to blog post once it's up.
---
+++ leetwindow/leetwindow/leetwindow.cpp
@@ -0,0 +1,16 @@
+// leetwindow.cpp : Defines the entry point for the application.
+//
+
+#include <windows.h>
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	MessageBoxA(0, "@primal0xF7", "simple_hollow", 0);
+
+	return 0;
+}
+	
